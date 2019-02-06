cbbaa2624e536a49d847c782ee4022bcba4438e5
Dmitriy Root <droot@deeptown.org>
2019-02-06 23:34:57 +0700

    Add monitors.py

--- devices/lib/pidev/monitor.py
+++ devices/lib/pidev/monitor.py
@@ -13,11 +13,19 @@ import pigpio
 class SensorsMonitor:
 
     _temp_timeout = 0.5
-    _temp_max = 100
+    _temp_max_len = 100
+    _water_capacity_j = 4185
+
+    class Calorimeter:
+        def __init__(self, temp_hi, temp_lo, flow):
+            self.temp_hi = temp_hi
+            self.temp_lo = temp_lo
+            self.flow = flow
 
     def __init__(self, pi, config):
         self._temp = {}
         self._flow = {}
+        self._calorimeter = {}
         self._temp_results = {}
         if (not config["sensors"]) or (not isinstance(config["sensors"], list)):
             return
@@ -60,6 +68,7 @@ class SensorsMonitor:
             def __init__(self):
                 self.temp = {}
                 self.flow = {}
+                self.consumption = {}
 
         result = Result()
 
@@ -75,6 +84,17 @@ class SensorsMonitor:
                 result.temp[name] = statistics.mean(self._temp_results[name])
                 self._temp_results.clear()
 
+        # 3. Fill in the energy consumption measurements
+        for name, calorimeter in self._calorimeter.items():
+            if (calorimeter.temp_hi in result.temp) and (calorimeter.temp_lo in result.temp):
+                temp_hi = result.temp[calorimeter.temp_hi]
+                temp_lo = result.temp[calorimeter.temp_lo]
+                if (temp_lo < -10) or (temp_hi > 110) or (temp_lo > temp_hi):
+                    logger.warning("unexpected temperatures on calorimeter, temp_hi: %f, temp_lo: %f (ignored)" % (temp_hi, temp_lo))
+                    continue
+                flow = result.flow[calorimeter.flow]
+                result.consumption[name] = SensorsMonitor._water_capacity_j * (temp_hi - temp_lo) * flow
+
         return result
 
     def _fill(self, pi, sensors):
@@ -84,12 +104,15 @@ class SensorsMonitor:
                 continue
             name = item['name']
             if not name:
-                name = ('sensor%02d' % (index++))
+                name = ('sensor%02d' % index)
+                index += 1
             type = item['type']
             if type == 'temp':
                 self._fill_temp(name, item)
             elif type == 'flow':
                 self._fill_flow(name, pi, item)
+            elif type == 'calorimeter':
+                self._fill_calorimeter(name, item)
 
     def _fill_temp(self, name, item):
         device_id = item['device_id']
@@ -116,6 +139,18 @@ class SensorsMonitor:
         logging.info("adding flow sensor name: '%s', pin: %d" % (name, pin))
         self._flow[name] = flow.FlowSensor(pi, pin)
 
+    def _fill_calorimeter(self, name, item):
+        temp_hi = item['temp_hi']
+        temp_lo = item['temp_lo']
+        flow = item['flow']
+        if (not temp_hi in self._temp) or (not temp_lo in self._temp) or (not flow in self._flow):
+            logging.warning("wrong arguments specified for calorimeter: '%s' (ignored)" % name)
+            return
+        if name in self._calorimeter:
+            logging.warning("duplicate name for calorimeter: '%s', ignored" % name)
+            return
+        self._calorimeter[name] = Calorimeter(temp_hi, temp_lo, flow)
+
     def _run(self):
         while True:
             for name, sensor in self._temp.items():
@@ -128,4 +163,5 @@ class SensorsMonitor:
                         return
                     if temp != None:
                         self._temp_results[name].append(temp)
+                        assert len(self._temp_results[name]) < SensorsMonitor._temp_max_len
                 time.sleep(SensorsMonitor._temp_timeout)
